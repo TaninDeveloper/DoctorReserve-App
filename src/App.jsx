f6d0f26d675ebd43875923a07823c8aafc2e31e6
tanin <taninsol@gmail.com>
2026-07-21 05:13:21 +0330

    Fix: use HashRouter for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import {Toaster} from 'react-hot-toast';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Doctors from './pages/Doctors';
